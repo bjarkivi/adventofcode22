cd295a3816320f393309a228b76d4dac5f784ca5
Bjarki Vilmarsson <bjarkivi@users.noreply.github.com>
2022-12-02 19:17:59 +0100

    Day 2 part 2

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -2,7 +2,12 @@
 Opponent: A = Rock, B = Paper, C=Scissors
 Me: X=Rock, Y=Paper, Z=Scissors
 Rock=1p, Paper=2p, Scissors=3p
-Win=6p, Lost=0p, Draw=3p*/
+Win=6p, Lost=0p, Draw=3p
+
+Part 2
+X means lose
+Y means draw
+Z means win*/
 
 #include <iostream>
 #include <string>
@@ -14,7 +19,7 @@ int main() {
     std::ifstream file("input.txt");
     std::string str;
     int score = 0;
-    std::map<std::string,int> options = {{"A", 1}, {"B",2}, {"C",3}, {"X",1}, {"Y",2}, {"Z",3}, {"A Y", 6}, {"A Z", 0}, {"B X", 0}, {"B Z", 6}, {"C X", 6}, {"C Y", 0}, {"A X", 3}, {"B Y", 3}, {"C Z", 3}};
+    std::map<std::string,int> options = {{"A", 1}, {"B",2}, {"C",3}, {"X",0}, {"Y",3}, {"Z",6}, {"A Y", 1}, {"A Z", 2}, {"A X", 3}, {"B X", 1}, {"B Y", 2}, {"B Z", 3}, {"C X", 2}, {"C Y", 3}, {"C Z", 1}};
     while(std::getline(file,str)) {
         std::string safePoints = str.substr(2,1);
         score+=options[safePoints];
