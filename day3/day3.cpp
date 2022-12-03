e108f326b2a3e74f7d6263d246c59bfcade820fc
Bjarki Vilmarsson <bjarkivi@users.noreply.github.com>
2022-12-03 15:58:32 +0100

    day3, part2
    Resuing variables, don't have time to refactor today.

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -27,30 +27,36 @@ int main() {
     std::string str;
     std::map<char,int> duplicates;
     int score = 0;
-    std::set<char> set1;
-    std::set<char> set2;
+    
     std::set<char> intersect;
+    std::set<char> intersect2;
     std::string firstHalf;
     std::string secondHalf;
     while(std::getline(file,str)) {
-        firstHalf = str.substr(0,str.length()/2);
-        secondHalf = str.substr(str.length()/2);
-        for(int i = 0; i<firstHalf.length(); ++i) {
-            set1.insert(firstHalf[i]);
-            set2.insert(secondHalf[i]);
-        }
+        
+        //firstHalf = str.substr(0,str.length()/2);
+        //secondHalf = str.substr(str.length()/2);
+        std::getline(file,firstHalf);
+        std::getline(file,secondHalf);
+        std::set<char> set1(std::begin(str), std::end(str));
+        std::set<char> set2(std::begin(firstHalf), std::end(firstHalf));
+        std::set<char> set3(std::begin(secondHalf), std::end(secondHalf));
         
         std::set_intersection(set1.begin(), set1.end(), set2.begin(), set2.end(),
                  std::inserter(intersect, intersect.begin()));
-        for(auto it = intersect.begin(); it !=
-                           intersect.end(); ++it)
+        std::set_intersection(intersect.begin(), intersect.end(), set3.begin(), set3.end(),
+                 std::inserter(intersect2, intersect2.begin()));
+        for(auto it = intersect2.begin(); it !=
+                           intersect2.end(); ++it)
         {
             //std::cout << *it << std::endl;
             checkLetter((int)*it,score, 1 );
         }
         set1.clear();
         set2.clear();
+        set3.clear();
         intersect.clear();
+        intersect2.clear();
         //for(int i =0; i<str.length();++i) {
         //    duplicates[str[i]]++;
         //}
